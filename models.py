96431d9c43c723db763083438ac1d335595389f5
BiancaArantes28 <biancaarantes28@gmail.com>
2017-04-12 19:23:37 -0300

    Aulas 9 e 10

--- models.py
+++ models.py
@@ -1,12 +1,25 @@
 # -*- coding: UTF-8 -*- 
 
-class Perfil():
+class Perfil(object):
 	'Classe para padrao perfis de usuario'
 
 	def __init__(self, nome, telefone, empresa):
 		self.nome = nome
 		self.telefone = telefone
 		self.empresa = empresa
+		self.__curtidas = 0
 
 	def imprimir(self):
-		print 'Nome: %s, Telfone %s, Empresa %s' % (self.nome,self.telefone,self.empresa)
+		print 'Nome: %s, Telfone %s, Empresa %s, Curtidas: %s' % (self.nome,self.telefone,self.empresa,self__curtidas)
+		
+	def curtir(self):
+		self.__curtidas+=1
+
+	def obter_curtidas(self):
+		return self.__curtidas
+
+class Perfil_Vip(Perfil):
+	'Classe para padrao perfis de usuario'
+
+	def obter_creditos(self):
+		return super(Perfil_Vip).__curtidas * 10
