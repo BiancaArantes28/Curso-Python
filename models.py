701277699936da426c20659e22fc6ec92808ab3f
BiancaArantes28 <biancaarantes28@gmail.com>
2017-04-16 21:17:59 -0300

    Conclusão do curso

--- models.py
+++ models.py
@@ -18,10 +18,22 @@ class Perfil(object):
 	def obter_curtidas(self):
 		return self.__curtidas
 
+	@classmethod
+	def gerar_perfis(classe, nome_arquivo):
+		arquivo = open(nome_arquivo,'r')
+		perfis = []
+		for linha in arquivo:
+			valores = linha.split(',')
+			if(len(valores) is not 3):
+				raise ValueError('Uma linha do arquivo %s nao possui 3 valores' % nome_arquivo)
+			perfis.append(classe(*valores))
+		arquivo.close()
+		return perfis
+
 class Perfil_Vip(Perfil):
 	'Classe para padrao perfis de usuario'
 
-	def __init__(self, nome, telefone, empresa, apelido):
+	def __init__(self, nome, telefone, empresa, apelido = ''):
 		super(Perfil_Vip, self).__init__(nome,telefone,empresa)
 		self.apelido = apelido
 
